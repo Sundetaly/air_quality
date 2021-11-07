2c14c0d8465644349b3f2e684ea5ecd586a7863d
Dastan <zhapaidastan@gmail.com>
2021-11-07 22:46:32 +0600

    added edit functionality

--- static/assets/dist/js/city.js
+++ static/assets/dist/js/city.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'http://0.0.0.0:8000/main'
+let cityList = []
 
 
 function clearFormInput (form) {
@@ -9,15 +10,18 @@ function clearFormInput (form) {
         .prop('selected', false);
 }
 
-function loadCityList(success, error) {
+function loadCityList() {
     $.ajax({
         url: `${ BASE_URL }/city`,
         type: 'GET',
-        dataType: 'json', // added data type
+        dataType: 'json',
         success: function(res) {
+            cityList = res
             res.forEach((i) => addCitiesTableRow(i))
         },
         error: function (err) {
+            cityList = []
+            clearCitiesTable()
             console.log(err)
         }
     });
@@ -34,7 +38,7 @@ function addCitiesTableRow(data) {
     cols += `<td>${ data.pollution_level_02 }</td>`;
     cols += `<td>${ data.industries }</td>`;
     cols += '<td>';
-    cols +=     `<button data-id="${ data.id }" type="button" class="btn btn-primary">Edit</button>`;
+    cols +=     `<button data-id="${ data.id }" type="button" class="btn btn-primary edit-item-button">Edit</button>`;
     cols +=     `<button data-id="${ data.id }" type="button" class="btn btn-danger ml-3 delete-item-button">Delete</button>`;
     cols += '</td>';
 
@@ -43,15 +47,18 @@ function addCitiesTableRow(data) {
     return false;
 };
 
+function clearCitiesTable() {
+    $(".table-cities tr").remove();
+}
 
 $( document ).ready(function() {
     loadCityList()
-    $('.save-changes-button').on('click', function (e) {
-        let name = $('#name').val();
-        let pollution_level_00 = $('#pollution_level_00').val();
-        let pollution_level_01 = $('#pollution_level_01').val();
-        let pollution_level_02 = $('#pollution_level_02').val();
-        let industries = $('#industries').val();
+    $('.save-create-changes-button').on('click', function (e) {
+        let name = $('#create_name').val();
+        let pollution_level_00 = $('#create_pollution_level_00').val();
+        let pollution_level_01 = $('#create_pollution_level_01').val();
+        let pollution_level_02 = $('#create_pollution_level_02').val();
+        let industries = $('#create_industries').val();
         $.ajax({
             method: "POST",
             url: `${ BASE_URL }/city/`,
@@ -63,10 +70,11 @@ $( document ).ready(function() {
                 industries
             }})
             .done(function( data ) {
-                clearFormInput('#createForm')
                 $('.modal').modal('hide');
+                clearFormInput('#createForm')
+                cityList.push(data)
                 addCitiesTableRow(data)
-            });
+        });
     })
 
     $(document).on('click', '.delete-item-button', function(event){
@@ -79,5 +87,47 @@ $( document ).ready(function() {
         .done(function() {
             button.parent().parent().remove()
         });
+    });
+
+    $(document).on('click', '.edit-item-button', function(event){
+        let id = $(this).attr('data-id');
+        let modal = $('#editModal');
+        modal.attr('data-id', id)
+        const city = cityList.find(i => String(i.id) === id)
+        if(!city) return
+
+        modal.find('#edit_name').val(city.name)
+        modal.find('#edit_pollution_level_00').val(city.pollution_level_00)
+        modal.find('#edit_pollution_level_01').val(city.pollution_level_01)
+        modal.find('#edit_pollution_level_02').val(city.pollution_level_02)
+        modal.find('#edit_industries').val(city.industries)
+        modal.modal('show')
+    });
+
+    $(document).on('click', '.save-edit-changes-button', function(event){
+        let modal = $('#editModal')
+        let id = modal.attr('data-id')
+        let name = $('#edit_name').val();
+        let pollution_level_00 = $('#edit_pollution_level_00').val();
+        let pollution_level_01 = $('#edit_pollution_level_01').val();
+        let pollution_level_02 = $('#edit_pollution_level_02').val();
+        let industries = $('#edit_industries').val();
+        $.ajax({
+            method: "PUT",
+            url: `${ BASE_URL }/city/${ id }/`,
+            data: {
+                name,
+                pollution_level_00,
+                pollution_level_01,
+                pollution_level_02,
+                industries
+            }})
+            .done(function( data ) {
+                modal.attr('data-id', '')
+                clearCitiesTable()
+                loadCityList()
+                $('.modal').modal('hide');
+                clearFormInput('#createForm')
+            });
     });
 });
