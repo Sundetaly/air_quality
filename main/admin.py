d444f0f19da3c79242a29d30116506646c9e1eda
Sundetali <sundetaliomirzak@gmail.com>
2022-02-08 21:26:08 +0600

    add filter to Admin page

--- main/admin.py
+++ main/admin.py
@@ -13,6 +13,7 @@ admin.site.unregister(Group)
 @admin.register(Pesticide)
 class PesticideAdmin(admin.ModelAdmin):
     list_display = ("region", "location", "count_pesticide", "name_pesticide", "name_banned_pesticide")
+    list_filter = ("region", "location", "count_pesticide", "name_pesticide", "name_banned_pesticide")
 
 
 admin.site.register(StorageStatus)
@@ -22,11 +23,13 @@ admin.site.register(Analysis)
 @admin.register(Storage)
 class StorageAdmin(admin.ModelAdmin):
     list_display = ("name", "region", "location", "status", "add_info", "obsolete_pesticides", "year")
+    list_filter = ("name", "region", "location", "status", "add_info", "obsolete_pesticides", "year")
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ("region", "number_rooms", "number_un_rooms", "number_limit_pdk", "number_above_pdk")
+    list_filter = ("region", "number_rooms", "number_un_rooms", "number_limit_pdk", "number_above_pdk")
 
 
 admin.site.register(FormPreparation)
@@ -37,7 +40,8 @@ admin.site.register(TypeContainer)
 class PreparationAdmin(admin.ModelAdmin):
     list_display = ("region", "location", "name_location", "trade_name", "number", "form", "type_container",
                     "status_container", "import_year", "birth", "add_info", "note")
-
+    list_filter = ("region", "location", "name_location", "trade_name", "number", "form", "type_container",
+                   "status_container", "import_year", "birth", "add_info", "note")
 
 
 
